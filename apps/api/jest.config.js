aa6e86af111e39d07c056f0a2ec57b0cfa10dfb1
useon <roqkftjs@gmail.com>
2026-09-08 19:14:05 +0900

    test: 카페 검색 정책 유닛 테스트 추가

---
+++ apps/api/jest.config.js
@@ -0,0 +1,11 @@
+const config = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: ".",
+  testEnvironment: "node",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+};
+
+module.exports = config;
